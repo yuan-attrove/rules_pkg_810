01dd9e8d8a58d9705b2046b217f79b8838743f94
Yuán <yuan@gettrove.co>
2024-01-11 11:49:00 -0800

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/yuan-attrove/rules_pkg_810
+
+go 1.21
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/yuan-attrove/rules_pkg_810/proto"
+)
+
+func main() {
+	m := proto.MyMessage{}
+
+	fmt.Println(m)
+}
